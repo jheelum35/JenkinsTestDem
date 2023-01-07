72ea1748eb776353edd11bccd539cf6d63661876
jheelum35 <ggithub7@gmail.com>
2023-01-07 16:25:55 +0530

    commit

--- Test/src/main/java/utils/BaseTest.java
+++ Test/src/main/java/utils/BaseTest.java
@@ -1,5 +1,6 @@
 package utils;
 
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Properties;
@@ -9,17 +10,24 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
 
 import BrowserFactory.BrowserFactory;
 import BrowserFactory.IBrowser;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseTest {
 
 	private WebDriver driver;
 	private Properties prop ;
+	private Properties prop2;
 	
 
 	public Properties getProp() {
@@ -30,22 +38,37 @@ public class BaseTest {
 		this.prop = prop;
 	}
 	
-	@BeforeMethod
-	public void setup() {
-	
-		Properties prop = IOUtils
-				.loadProperties("//Users//anirban//Documents//JenkinsTestDem//Test//Config.properties");
-		BrowserFactory browserFactory = new BrowserFactory();
-		IBrowser browser = browserFactory.getBrowserType(prop.getProperty("Browser"));
-		driver = browser.getBrowser();
-		System.out.println("Build Number is ------>"+prop.getProperty("jenkins.build.number"));
-				System.out.println(prop.getProperty("URL"));
-		driver.manage().window().maximize();
-		driver.get(prop.getProperty("URL"));
-
+	public Properties getProp2() {
+		return prop2;
 	}
 
-	@AfterMethod
+	public void setProp2(Properties prop2) {
+		this.prop2 = prop2;
+	}
+
+	@BeforeMethod
+	public void setup() throws IOException {
+
+		prop2 = new Properties();
+		
+		prop2.load(BaseTest.class.getClassLoader().getResourceAsStream("Config.properties"));
+		//Properties prop = IOUtils
+			//	.loadProperties("//Users//anirban//Documents//JenkinsTestDem//Test//Config.properties");
+		BrowserFactory browserFactory = new BrowserFactory();
+		IBrowser browser = browserFactory.getBrowserType(prop2.getProperty("Browser"));
+		driver = browser.getBrowser();
+		System.out.println("Build Number is ------>"+prop2.getProperty("jenkins.build.number"));
+				System.out.println(prop2.getProperty("URL"));
+		driver.manage().window().maximize();
+		driver.get(prop2.getProperty("URL"));
+
+	}
+@AfterClass
+public void Endsetup()
+{
+	
+}
+@AfterMethod
 	public void teardown() {
 		getDriver().quit();
 	}
--- Test/src/test/java/RegressionTest/Test2.java
+++ Test/src/test/java/RegressionTest/Test2.java
@@ -1,5 +1,21 @@
 package RegressionTest;
 
-public class Test2 {
+import java.io.IOException;
+import java.util.Properties;
 
+import org.testng.annotations.Test;
+
+import utils.BaseTest;
+
+public class Test2 extends BaseTest{
+
+	@Test
+	public void getData() throws IOException
+	{
+		Properties prop = new Properties();
+		
+		prop.load(Test2.class.getClassLoader().getResourceAsStream("Config.properties"));
+		System.out.println(prop.getProperty("uname"));
+		System.out.println(prop.getProperty("pword"));
+	}
 }
